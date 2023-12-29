6187a4574ccfa86794b62b95f3d66e651d395147
prajakta1003 <contact.prajaktajamdar@gmail.com>
2023-12-29 11:10:51 +0530

    first commit

---
+++ src/components/SelectButton.jsx
@@ -0,0 +1,35 @@
+import { BoxSelect } from "lucide-react";
+import React from "react";
+
+const SelectButton = ({ tool, setTool }) => {
+  return (
+    <div>
+      <input
+        type="radio"
+        id="selection"
+        checked={tool === "selection"}
+        onChange={() => setTool("selection")}
+        style={{ display: "none" }}
+      />
+      <label
+        htmlFor="selection"
+        style={{
+          fontSize: "14px",
+          display: "flex",
+          padding: "5px 10px",
+          color: "white",
+          borderRadius: "8px",
+          backgroundColor: "#1a56db",
+          alignItems: "center",
+          marginLeft: "10px",
+          gap: "10px",
+          cursor: "pointer", // set cursor to pointer for better UX
+        }}
+      >
+        <BoxSelect size={19} className="lucide-icon" /> Selection
+      </label>
+    </div>
+  );
+};
+
+export default SelectButton;
